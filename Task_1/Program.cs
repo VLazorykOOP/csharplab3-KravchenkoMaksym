b376597010389836d872a7c7b311d4f392721596
Maksym Kravchenko <kravchenko.maksym@chnu.edu.ua>
2024-03-05 14:16:49 +0200

    Lab_3

---
+++ Task_1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+
+namespace Task_1
+{
+    class ATriangle
+    {
+        // Поля
+        protected int a;
+        protected int b;
+        private readonly string color; // колір трикутника
+
+        // Конструктор
+        public ATriangle(int sideA, int sideB, string triangleColor)
+        {
+            a = sideA;
+            b = sideB;
+            color = triangleColor;
+        }
+
+        // Методи
+        public void PrintSides()
+        {
+            Console.WriteLine($"Сторони трикутника: a = {a}, b = {b}, гiпотенуза = {GetHypotenuse()}");
+        }
+
+        public double CalculatePerimeter()
+        {
+            return a + b + GetHypotenuse();
+        }
+
+        public double CalculateArea()
+        {
+            return 0.5 * a * b;
+        }
+
+        public bool IsIsosceles()
+        {
+            return a == b || a == GetHypotenuse() || b == GetHypotenuse();
+        }
+
+        // Властивості
+        public int SideA
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public int SideB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+        }
+
+        // Приватний метод для розрахунку гіпотенузи
+        private double GetHypotenuse()
+        {
+            return Math.Sqrt(a * a + b * b);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введiть сторону a:");
+            int sideA = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введiть сторону b:");
+            int sideB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введiть колiр трикутника:");
+            string triangleColor = Console.ReadLine();
+
+            ATriangle triangle = new ATriangle(sideA, sideB, triangleColor);
+
+            Console.WriteLine("Iнформацiя про трикутник:");
+            triangle.PrintSides();
+            Console.WriteLine($"Периметр: {triangle.CalculatePerimeter()}");
+            Console.WriteLine($"Площа: {triangle.CalculateArea()}");
+            Console.WriteLine($"Чи є рiвнобедреним: {triangle.IsIsosceles()}");
+            Console.WriteLine($"Колiр трикутника: {triangle.Color}");
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine($"Товар: {Name}, Цiна: {Price}");
+        }
+    }
+
+    class Toy : Product
+    {
+        public string AgeRange { get; set; }
+
+        public Toy(string name, double price, string ageRange)
+            : base(name, price)
+        {
+            AgeRange = ageRange;
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine($"Вiковий дiапазон: {AgeRange}");
+        }
+    }
+
+    class DairyProduct : Product
+    {
+        public DateTime ExpiryDate { get; set; }
+
+        public DairyProduct(string name, double price, DateTime expiryDate)
+            : base(name, price)
+        {
+            ExpiryDate = expiryDate;
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine($"Термiн придатностi: {ExpiryDate.ToShortDateString()}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Product[] products = new Product[3];
+
+            products[0] = new Toy("Ведмiдь", 15.99, "3-6 рокiв");
+            products[1] = new DairyProduct("Молоко", 2.49, DateTime.Now.AddMonths(1));
+            products[2] = new Product("Книга", 9.99);
+
+            ShowProducts(products);
+        }
+
+        static void ShowProducts(Product[] products)
+        {
+            foreach (var product in products)
+            {
+                product.Show();
+                Console.WriteLine();
+            }
+        }
+    }
+}
